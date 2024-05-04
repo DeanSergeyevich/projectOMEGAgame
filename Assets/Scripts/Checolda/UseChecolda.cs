40dcb6bb7542f85a0a5ac0c77370ce5d49500878
DeanSergeyevich <109577115+DeanSergeyevich@users.noreply.github.com>
2024-05-04 19:54:19 +0300

    Пофиксил щеколды, добавлены объекты и освещение.

--- Assets/Scripts/Checolda/UseChecolda.cs
+++ Assets/Scripts/Checolda/UseChecolda.cs
@@ -1,51 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UseChecolda : MonoBehaviour
 {
-
-    public GameObject checolda;
-    public bool isChecoldaUsed = false;
-    public Animator Checolda;
-    public Camera playerCamera; // Камера игрока
+    public GameObject[] checoldas; // Массив щеколд
+    public Camera playerCamera;
     public LayerMask interactChecolda;
     public float interactionDistance = 2f;
     public float maxAngle = 30f; // Максимальный угол между направлением взгляда и направлением к щеколде
 
-    private void Start()
-    {
-        Checolda = GetComponent<Animator>();
-        if(Checolda == null) 
-        {
-            Debug.LogError("Animator не найден!");
-        }
-        
-    }
-
-
-    // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.E) && CanOpenChecolda())
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            bool isChecoldaUsed = Checolda.GetBool("isChecoldaUsed");
-            Checolda.SetBool("isChecoldaUsed", !isChecoldaUsed);
+            foreach (GameObject checolda in checoldas)
+            {
+                if (CanOpenChecolda(checolda))
+                {
+                    Animator animator = checolda.GetComponent<Animator>();
+                    if (animator != null)
+                    {
+                        bool isChecoldaUsed = animator.GetBool("isChecoldaUsed");
+                        animator.SetBool("isChecoldaUsed", !isChecoldaUsed);
+                    }
+                }
+            }
         }
-
-
     }
 
-    public bool CanOpenChecolda()
+    public bool CanOpenChecolda(GameObject checolda)
     {
-        if (playerCamera == null) return false;
+        if (playerCamera == null || checolda == null) return false;
 
-        // Проверяем расстояние между игроком и щеколдой
         float distanceToChecolda = Vector3.Distance(playerCamera.transform.position, checolda.transform.position);
         if (distanceToChecolda > interactionDistance) return false;
 
-        // Проверяем направление взгляда камеры к щеколде
         Vector3 directionToChecolda = checolda.transform.position - playerCamera.transform.position;
         float angle = Vector3.Angle(playerCamera.transform.forward, directionToChecolda);
         if (angle > maxAngle) return false;
